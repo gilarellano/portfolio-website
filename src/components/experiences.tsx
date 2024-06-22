9fcdb4877e1d5e5ca5875fc7570fb075095c007f
Gilberto Arellano Lopez <giarellano@chapman.edu>
2024-06-22 08:17:21 -0700

    feat(projects/experience): Added hover effect, split up content and map onto div's

--- src/components/experiences.tsx
+++ src/components/experiences.tsx
@@ -1,47 +1,83 @@
+import exp from "constants";
 import React from "react";
 
-const ListOfExperiences = () => {
-    return (
-        <section id="experiences">
-            <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen bg-background px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
-              <h2 className="text-sm font-bold uppercase tracking-widest text-slate-200 lg:sr-only">Experience</h2>
-            </div>
+interface Experience {
+  href: string;
+  period: string;
+  role: string;
+  company: string;
+  description: string;
+  technologies?: string[];
+}
 
-            <div className="flex flex-col gap-y-12">
-            {/* Job 1 */}
-              <div className="flex flex-col gap-y-2 lg:flex-row lg:gap-x-4">
-                <p className="pt-0.5 lg:w-[86px] shrink-0 text-secondary-text font-light text-sm lg:text-xs">2014 - PRESENT</p>
+const experiences: Experience[] = [
+  {
+    href: "gilbertowindows.com",
+    period: "2014 - PRESENT",
+    role: "Operations Manager & Systems Developer",
+    company: "Gilberto Arellano Windows, San Francisco",
+    description:
+      "Managed daily operations, staffing, inventory, and customer service to ensure a high-quality dining experience. Collaborated on menu development, marketing strategies, and financial planning, driving significant growth and establishing our taqueria as a beloved local spot.",
+    technologies: ["CRM", "Quickbooks"],
+  },
+  {
+    href: "gilbertowindows.com",
+    period: "2016 - 2019",
+    role: "Restaurant Co-Owner",
+    company: "La Capilla, Berkeley",
+    description:
+      "Managed daily operations, staffing, inventory, and customer service to ensure a high-quality dining experience. Collaborated on menu development, marketing strategies, and financial planning, driving significant growth and establishing our taqueria as a beloved local spot.",
+  },
+];
+
+const ListOfExperiences: React.FC = () => {
+  return (
+    <section id="experiences" className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24">
+
+      <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen bg-background px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
+        <h2 className="text-sm font-bold uppercase tracking-widest text-slate-200 lg:sr-only">
+          Experience
+        </h2>
+      </div>
+
+      <ol className="group/list flex flex-col" role="list">
+        {experiences.map((experience, index) => (
+          <a key={index} href={experience.href}>
+            <li className="group relative mb-10 rounded-md transition-all hover:!opacity-100 group-hover/list:opacity-50">
+              <div className="absolute -inset-x-6 -inset-y-4 z-0 hidden lg:block rounded-md motion-reduce:transition-none group-hover:bg-slate-800/50 group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] group-hover:drop-shadow-lg"></div>
+              <div className="relative z-10 flex flex-col gap-y-2 lg:flex-row lg:gap-x-4">
+                <p className="pt-0.5 lg:w-[86px] shrink-0 text-secondary-text font-light text-sm lg:text-xs">
+                  {experience.period}
+                </p>
                 <div className="flex flex-col">
                   <div className="flex flex-col pb-2">
                     <div className="pb-2">
-                      <h3 className="text-primary-text text-2xl lg:text-xl font-bold">Operations Manager & Systems Developer</h3>
-                      <h4 className="text-secondary-text text-sm lg:text-xs font-bold">Gilberto Arellano Windows, San Francisco</h4>
+                      <h3 className="text-primary-text text-2xl lg:text-xl font-bold group-hover:text-primary">
+                        {experience.role}
+                      </h3>
+                      <h4 className="text-secondary-text text-sm lg:text-xs font-bold">
+                        {experience.company}
+                      </h4>
                     </div>
-                    <p className="text-secondary-text text-sm lg:text-xs font-base">Managed daily operations, staffing, inventory, and customer service to ensure a high-quality dining experience. Collaborated on menu development, marketing strategies, and financial planning, driving significant growth and establishing our taqueria as a beloved local spot.</p>
+                    <p className="text-secondary-text text-sm lg:text-xs">
+                      {experience.description}
+                    </p>
                   </div>
-                  <ul className="flex flex-grid pt-2 gap-3 text-primary text-sm lg:text-xs font-extralight *:rounded-full *:border *:border-green-950 *:bg-green-950 *:px-3 *:py-1">
-                    <li>CRM</li>
-                    <li>Quickbooks</li>
-                  </ul>
+                  {experience.technologies && (
+                    <ul className="flex flex-grid pt-2 gap-3 text-primary text-sm lg:text-xs font-extralight *:rounded-full *:border *:border-green-950 *:bg-green-950 *:px-3 *:py-1">
+                      {experience.technologies.map((tech, techIndex) => (
+                        <li key={techIndex}>{tech}</li>
+                      ))}
+                    </ul>
+                  )}
                 </div>
               </div>
-
-              {/* Job 2 */}
-              <div className="flex flex-col gap-y-2 lg:flex-row lg:gap-x-4">
-                <p className="pt-0.5 lg:w-[86px] shrink-0 text-secondary-text font-light text-sm lg:text-xs">2016 - 2019</p>
-                <div className="flex flex-col">
-                  <div className="flex flex-col pb-2">
-                    <div className="pb-2">
-                      <h3 className="text-primary-text text-2xl lg:text-xl font-bold">Restaurant Co-Owner</h3>
-                      <h4 className="text-secondary-text text-sm lg:text-xs font-bold">La Capilla, Berkeley</h4>
-                    </div>
-                    <p className="text-secondary-text text-sm lg:text-xs font-base">Managed daily operations, staffing, inventory, and customer service to ensure a high-quality dining experience. Collaborated on menu development, marketing strategies, and financial planning, driving significant growth and establishing our taqueria as a beloved local spot.</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </section>
-    );
+            </li >
+          </a>
+        ))}
+      </ol>
+    </section>
+  );
 };
 
 export default ListOfExperiences;
--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,123 +1,93 @@
 import React from "react";
 import Image from "next/image";
 
-const ListOfProjects = () => {
+interface Project {
+  href: string;
+  imgSrc: string;
+  imgAlt: string;
+  title: string;
+  description: string;
+  technologies: string[];
+}
+
+const projects: Project[] = [
+  {
+    href: "github.com",
+    imgSrc: "/project_screenshot.webp",
+    imgAlt: "Academic Advising Tool Screenshot",
+    title: "Academic Advising Tool",
+    description:
+      "Build and maintain critical components used to construct Klaviyo’s frontend, across the whole product. Work closely with cross-functional teams, including developers, designers, and product managers, to implement and advocate for best practices in web accessibility.",
+    technologies: ["AWS", "Typescript", "React", "Next.js", "Jest", "SonarQube"],
+  },
+  {
+    href: "#",
+    imgSrc: "/project_screenshot.webp",
+    imgAlt: "Window Quote Project Screenshot",
+    title: "Window Quote Project",
+    description:
+      "Build and maintain critical components used to construct Klaviyo’s frontend, across the whole product. Work closely with cross-functional teams, including developers, designers, and product managers, to implement and advocate for best practices in web accessibility.",
+    technologies: ["C++", "Documentation", "CI/CD"],
+  },
+  {
+    href: "#",
+    imgSrc: "/project_screenshot.webp",
+    imgAlt: "Speech Emulation Project Screenshot",
+    title: "Long Short Term Memory: Speech Emulation",
+    description:
+      "Build and maintain critical components used to construct Klaviyo’s frontend, across the whole product. Work closely with cross-functional teams, including developers, designers, and product managers, to implement and advocate for best practices in web accessibility.",
+    technologies: ["sci-kit learn", "API"],
+  },
+  {
+    href: "#",
+    imgSrc: "/project_screenshot.webp",
+    imgAlt: "Game Of Life Project Screenshot",
+    title: "Game Of Life: Showcasing OOP Practices",
+    description:
+      "Build and maintain critical components used to construct Klaviyo’s frontend, across the whole product. Work closely with cross-functional teams, including developers, designers, and product managers, to implement and advocate for best practices in web accessibility.",
+    technologies: ["C++"],
+  },
+];
+
+const ListOfProjects: React.FC = () => {
   return (
-    <section id="projects">
+    <section id="projects" className="mb-24 scroll-mt-16 lg:scroll-mt-24">
+      {/* Projects Header for smaller screens */}
       <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen bg-background px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
         <h2 className="text-sm font-bold uppercase tracking-widest text-slate-200 lg:sr-only">
           Projects
         </h2>
       </div>
-      <div className="flex flex-col gap-y-12 pb-36">
-        <div className="flex flex-row gap-x-4">
-          <Image
-            src="/project_screenshot.webp"
-            alt="Academic Advising Tool Screenshot"
-            width={86}
-            height={68}
-            className="mt-1 shrink-0 w-[86px] h-[68px] border-2 border-custom rounded-custom"
-          />
-          <div className="flex flex-col gap-y-2">
-            <h3 className="text-primary-text text-xl font-bold">
-              Academic Advising Tool
-            </h3>
-            <p className="text-secondary-text text-xs">
-              Build and maintain critical components used to construct Klaviyo’s
-              frontend, across the whole product. Work closely with
-              cross-functional teams, including developers, designers, and
-              product managers, to implement and advocate for best practices in
-              web accessibility.
-            </p>
-            <ul className="flex flex-grid flex-wrap pt-2 gap-3 text-primary text-xs font-extralight *:rounded-full *:border *:border-green-950 *:bg-green-950 *:px-3 *:py-1">
-              <li>AWS</li>
-              <li>Typescript</li>
-              <li>React</li>
-              <li>Next.js</li>
-              <li>Jest</li>
-              <li>SonarQube</li>
-            </ul>
-          </div>
-        </div>
 
-        <div className="flex flex-row gap-x-4">
-          <Image
-            src="/project_screenshot.webp"
-            alt="Academic Advising Tool Screenshot"
-            width={86}
-            height={68}
-            className="mt-1 shrink-0 w-[86px] h-[68px] border-2 border-custom rounded-custom"
-          />
-          <div className="flex flex-col gap-y-2">
-            <h3 className="text-primary-text text-xl font-bold">
-              Window Quote Project
-            </h3>
-            <p className="text-secondary-text text-xs">
-              Build and maintain critical components used to construct Klaviyo’s
-              frontend, across the whole product. Work closely with
-              cross-functional teams, including developers, designers, and
-              product managers, to implement and advocate for best practices in
-              web accessibility.
-            </p>
-            <ul className="flex flex-grid flex-wrap pt-2 gap-3 text-primary text-xs font-extralight *:rounded-full *:border *:border-green-950 *:bg-green-950 *:px-3 *:py-1">
-              <li>C++</li>
-              <li>Documentation</li>
-              <li>CI/CD</li>
-            </ul>
-          </div>
-        </div>
-
-        <div className="flex flex-row gap-x-4">
-          <Image
-            src="/project_screenshot.webp"
-            alt="Academic Advising Tool Screenshot"
-            width={86}
-            height={68}
-            className="mt-1 shrink-0 w-[86px] h-[68px] border-2 border-custom rounded-custom"
-          />
-          <div className="flex flex-col gap-y-2">
-            <h3 className="text-primary-text text-xl font-bold">
-              Long Short Term Memory: Speech Emulation
-            </h3>
-            <p className="text-secondary-text text-xs">
-              Build and maintain critical components used to construct Klaviyo’s
-              frontend, across the whole product. Work closely with
-              cross-functional teams, including developers, designers, and
-              product managers, to implement and advocate for best practices in
-              web accessibility.
-            </p>
-            <ul className="flex flex-grid flex-wrap pt-2 gap-3 text-primary text-xs font-extralight *:rounded-full *:border *:border-green-950 *:bg-green-950 *:px-3 *:py-1">
-              <li>sci-kit learn</li>
-              <li>API</li>
-            </ul>
-          </div>
-        </div>
-
-        <div className="flex flex-row gap-x-4">
-          <Image
-            src="/project_screenshot.webp"
-            alt="Academic Advising Tool Screenshot"
-            width={86}
-            height={68}
-            className="mt-1 shrink-0 w-[86px] h-[68px] border-2 border-custom rounded-custom"
-          />
-          <div className="flex flex-col gap-y-2">
-            <h3 className="text-primary-text text-xl font-bold">
-              Game Of Life: Showcasing OOP Practices
-            </h3>
-            <p className="text-secondary-text text-xs">
-              Build and maintain critical components used to construct Klaviyo’s
-              frontend, across the whole product. Work closely with
-              cross-functional teams, including developers, designers, and
-              product managers, to implement and advocate for best practices in
-              web accessibility.
-            </p>
-            <ul className="flex flex-grid flex-wrap pt-2 gap-3 text-primary text-xs font-extralight *:rounded-full *:border *:border-green-950 *:bg-green-950 *:px-3 *:py-1">
-              <li>C++</li>
-            </ul>
-          </div>
-        </div>
-      </div>
+      <ol className="group/list flex flex-col" role="list">
+        {projects.map((project, index) => (
+          <a key={index} href={project.href}>
+            <li className="group relative mb-12 rounded-md transition-all hover:!opacity-100 group-hover/list:opacity-50">
+              <div className="absolute -inset-x-6 -inset-y-4 z-0 hidden lg:block rounded-md motion-reduce:transition-none group-hover:bg-slate-800/50 group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] group-hover:drop-shadow-lg"></div>
+              <div className="relative z-10 flex flex-row gap-x-4">
+                <Image
+                  src={project.imgSrc}
+                  alt={project.imgAlt}
+                  width={86}
+                  height={68}
+                  className="mt-1 shrink-0 w-[86px] h-[68px] border-custom-width border-custom rounded-custom group-hover:border-emerald-300"
+                />
+                <div className="flex flex-col gap-y-2">
+                  <h3 className="text-primary-text text-2xl lg:text-xl font-bold group-hover:text-primary">
+                    {project.title}
+                  </h3>
+                  <p className="text-secondary-text text-sm lg:text-xs">{project.description}</p>
+                  <ul className="flex flex-grid flex-wrap pt-2 gap-3 text-primary text-sm lg:text-xs font-extralight *:rounded-full *:border *:border-green-950 *:bg-green-950 *:px-3 *:py-1">
+                    {project.technologies.map((tech, techIndex) => (
+                      <li key={techIndex}>{tech}</li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+            </li>
+          </a>
+        ))}
+      </ol>
     </section>
   );
 };
