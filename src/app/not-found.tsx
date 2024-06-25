873d7415ea8ffac1813edb536aa3e6ad2e6b1dc6
Gilberto Arellano Lopez <giarellano@chapman.edu>
2024-06-24 18:39:43 -0700

    feat: created not-found page, just a draft

---
+++ src/app/not-found.tsx
@@ -0,0 +1,14 @@
+// app/not-found.tsx
+
+import React from 'react';
+import Link from 'next/link';
+
+const NotFound = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+        <h1 className="text-4xl font-bold mb-4">404 - I only created one page</h1>
+    </div>
+  );
+};
+
+export default NotFound;
